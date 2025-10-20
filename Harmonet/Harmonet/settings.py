377497c00dd0572ea0f969f5372e075d6edc3ce3
nbeahm <nbeahms1@gmail.com>
2025-10-19 23:31:35 -0600

    Update settings.py
    Fixed database config

--- Harmonet/Harmonet/settings.py
+++ Harmonet/Harmonet/settings.py
@@ -81,12 +81,11 @@ WSGI_APPLICATION = 'Harmonet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DDATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
