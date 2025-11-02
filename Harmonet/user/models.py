e1e520cc8b4931825b4a556b0272d9df3001c493
Peter D Dunbar <141361003+Peter-Dunbar@users.noreply.github.com>
2025-11-01 20:29:16 -0600

    Update models.py

--- Harmonet/user/models.py
+++ Harmonet/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class SoundCloudArtist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='soundcloud_artists')
+    name = models.CharField(max_length=255)
+    profile_url = models.URLField(blank=True, null=True)
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.user.username})"
