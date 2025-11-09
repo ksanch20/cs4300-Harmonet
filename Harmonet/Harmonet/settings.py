14a5ede78fd181533c4b370afaf502ab65de233e
Peter D Dunbar <141361003+Peter-Dunbar@users.noreply.github.com>
2025-11-08 23:39:57 -0700

    Update settings.py

--- Harmonet/Harmonet/settings.py
+++ Harmonet/Harmonet/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Spotify variables and client ID
 SPOTIPY_CLIENT_ID = "9b551f0dbef84c38b0e3dd1790d8752e"
 SPOTIPY_CLIENT_SECRET = "fc4e30448e214be285df5707a19237d5"
+#for testing use: https://cs4300-harmonet-accj.onrender.com  || https://cs4300-harmonet-accj.onrender.com/user/spotify/callback/
 SPOTIPY_REDIRECT_URI = "https://harmonets.org/user/spotify/callback/"
 
 ALLOWED_HOSTS = [
