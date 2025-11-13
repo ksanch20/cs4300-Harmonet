47326d8ab859a25b1fa94b91d08b8443fc7a4a13
Peter D Dunbar <141361003+Peter-Dunbar@users.noreply.github.com>
2025-11-12 18:06:29 -0700

    Update settings.py

--- Harmonet/Harmonet/settings.py
+++ Harmonet/Harmonet/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'Harmonet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.parse('postgresql://harmonet_database_user:qel7JegasDb20m2GztJp09ySNVIDmkEz@dpg-d4aigq1e2q1c73b0l70g-a.oregon-postgres.render.com/harmonet_database')}
+DATABASES = {'default': dj_database_url.parse('postgresql://testdatabase_hhx7_user:9aWdqjNOLHfxUI1P1XETkbuvD9kGWoNe@dpg-d4aitdeuk2gs739g4ekg-a.oregon-postgres.render.com/testdatabase_hhx7')}
 
 # Override for tests - use SQLite in-memory
 if 'test' in sys.argv:
