4e231df03b0ec1f03e2a621c2648a1cb448917b1
MonarchDoge <jackdodge12@gmail.com>
2025-11-16 11:37:39 -0700

    Changed from top_spotify_artists to spotify_top_artists

--- Harmonet/user/tests.py
+++ Harmonet/user/tests.py
@@ -736,7 +736,7 @@ class AIServiceUnitTests(TestCase):
             'manual_artists': [],
             'manual_genres': [],
             'manual_tracks': [],
-            'top_spotify_artists': []
+            'spotify_top_artists': []
         }
 
         with patch('user.ai_service.gather_user_music_data', return_value=mock_data), \
@@ -775,7 +775,7 @@ class AIServiceUnitTests(TestCase):
             'manual_artists': ['Radiohead'],
             'manual_genres': ['Alternative'],
             'manual_tracks': ['Karma Police', 'No Surprises'],
-            'top_spotify_artists': ['Muse']
+            'spotify_top_artists': ['Muse']
         }
         prompt = build_recommendation_prompt(music_data)
         self.assertIn('**Favorite Artists:**', prompt)
